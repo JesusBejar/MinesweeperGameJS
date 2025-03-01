667e0af0e38535a17312c70f830e64a60263c742
Jesus Bejar <israelbejarii@gmail.com>
2025-03-01 11:02:49 -0700

    added create element & tile status code

--- minesweeper.js
+++ minesweeper.js
@@ -1,11 +1,23 @@
 // backend game logic
 
+const TILE_STATUSES = {
+    HIDDEN: 'hidden', 
+    MINE: "mine", 
+    NUMBER: "number",
+    MARKED: "marked"
+}
+
 export function createBoard (boardSize, numOfMines) {
     const board = []
     for (let x = 0; x < boardSize; x++) {
         const row = []
         for (let y = 0; y < boardSize; y++) {
+            // create tiles
+            const element = document.createElement("div")
+            // hide tiles
+            element.dataset.status = TILE_STATUSES.HIDDEN
             const tile = {
+                element, 
                 x, 
                 y
             }
