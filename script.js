c6877287ea468a963d5c889725b3cbc9081dba76
Jesus Bejar <israelbejarii@gmail.com>
2025-03-01 10:54:36 -0700

    made createBoard func, added more comments

---
+++ script.js
@@ -0,0 +1,9 @@
+import { createBoard } from "./minesweeper.js"
+// frontend display code
+
+console.log(createBoard(2, 2))
+// Populate the board with tiles/mines
+
+// add left (reveal tiles) & right click (mark tiles) functionality
+
+// win/lose check
